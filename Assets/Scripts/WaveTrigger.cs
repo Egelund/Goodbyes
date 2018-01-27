bbcf3d8ffe5b154bf07b32e18c7eb7eb3d4bb60b
Fabian Dominguez <devfabidominguez@gmail.com>
2018-01-27 12:16:34 -0300

    WaveTrigger

---
+++ Assets/Scripts/WaveTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveTrigger : MonoBehaviour
+{
+
+
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.tag == "Wave")
+		{
+			Debug.Log("TriggerEnter2D");
+			RedirectWave();
+		}
+	}
+
+	private void RedirectWave()
+	{
+		//Add wave parameter
+		//Redirect wave vector
+		Debug.Log("Redirecting Wave");
+	}
+}
