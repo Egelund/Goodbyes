d2ef19b2008d0061edcfa5d9e27fb7cf619dfef1
Fabian Dominguez <devfabidominguez@gmail.com>
2018-01-27 17:19:04 -0300

    missing meta

--- Assets/Scripts/WaveShooter.cs
+++ Assets/Scripts/WaveShooter.cs
@@ -7,27 +7,6 @@ public class WaveShooter : MonoBehaviour {
 	[HideInInspector]
 	public Rigidbody2D rigidBody;
 
-	public GameObject wavePrefab;
-
-	// Use this for initialization
-	void Start () {
-	}
-
-	Vector3 savedVelocity;
-	Vector3 savedAngularVelocity;
-
-	void OnPauseGame()
-	{
-		//savedVelocity = rigidBody.velocity;
-		//savedAngularVelocity = rigidBody.angularVelocity;
-	}
-
-	void OnResumeGame()
-	{
-		//rigidBody.AddForce(savedVelocity, ForceMode.VelocityChange);
-		//rigidBody.AddTorque(savedAngularVelocity, ForceMode2D.Impulse);
-	}
-
 	public void Shoot(Quaternion newRotation)
 	{
 		rigidBody = GetComponent<Rigidbody2D>();
