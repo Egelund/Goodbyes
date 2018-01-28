b27a8e2fd2e5264a290c284f10047dfe8a1b4995
Fabian Dominguez <devfabidominguez@gmail.com>
2018-01-28 10:59:06 -0300

    Level1 Fixed Wall fixed

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -2,22 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Wall : MonoBehaviour {
+public class Wall : MonoBehaviour
+{
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start()
+	{
+
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+
 	}
 
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
+
+		if (collision.gameObject != null)
+		{
+			if (collision.gameObject.transform.parent.gameObject == null)
+			{
+				return;
+			}
+		}
+
 		Wave wave = collision.gameObject.transform.parent.gameObject.GetComponent<Wave>();
-		if(wave != null)
+		if (wave != null)
 		{
 			wave.DestroyWave();
 		}
